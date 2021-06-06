cd65469354f23011418e8a17fe849d0d5fa3b8e6
dmirci2@gmail.com <dmirci2@gmail.com>
2021-06-07 00:00:00 +0300

    Category - Car ilişkisi

--- car/admin.py
+++ car/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from car.models import Category
+from car.models import Category, Car
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
 
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'price', 'color', 'status']
+    list_filter = ['status' , 'category']
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Car, CarAdmin)
--- car/models.py
+++ car/models.py
@@ -13,9 +13,37 @@ class Category(models.Model) :
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField()
-    parent_id = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+
+class Car(models.Model):
+    STATUS = {
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    }
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40)
+    keywords = models.CharField(max_length=260)
+    description = models.CharField(blank=True, max_length=260)
+    image = models.ImageField(blank=True, upload_to='images/')
+    brand = models.CharField(max_length=15)
+    year = models.IntegerField()
+    vites = models.CharField(max_length=10)
+    price = models.FloatField()
+    fuel = models.CharField(max_length=260)
+    engine = models.CharField(max_length=260)
+    color = models.CharField(max_length=20)
+    seats = models.IntegerField()
+    luggage = models.CharField(max_length=10)
+    detail = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField(blank=True, max_length=150)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
