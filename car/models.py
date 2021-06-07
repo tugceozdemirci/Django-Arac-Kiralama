2db1eb45eee1aab879f986b3ae5e82ca494149b3
dmirci2@gmail.com <dmirci2@gmail.com>
2021-06-07 13:19:03 +0300

    Image Upload İşlemi

--- car/models.py
+++ car/models.py
@@ -32,11 +32,11 @@ class Car(models.Model):
     keywords = models.CharField(max_length=260)
     description = models.CharField(blank=True, max_length=260)
     image = models.ImageField(blank=True, upload_to='images/')
-    brand = models.CharField(max_length=15)
+    brand = models.CharField(max_length=20)
     year = models.IntegerField()
     vites = models.CharField(max_length=10)
     price = models.FloatField()
-    fuel = models.CharField(max_length=260)
+    fuel = models.CharField(max_length=10)
     engine = models.CharField(max_length=260)
     color = models.CharField(max_length=20)
     seats = models.IntegerField()
@@ -47,3 +47,5 @@ class Car(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
